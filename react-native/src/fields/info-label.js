42a8909c969399556794af8e2cdfd4c0d5c0b5e8
Michael Boreham <bor3ham@gmail.com>
2020-02-05 16:58:01 +1100

    Improve info label

--- react-native/src/fields/info-label.js
+++ react-native/src/fields/info-label.js
@@ -1,73 +1,27 @@
-import React from 'react'
-import { Text, View, TouchableHighlight, Modal, ScrollView, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  TouchableHighlight,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { Button } from '../layout'
 
-class InfoLabel extends React.PureComponent {
-  state = {
-    modalOpen: false,
-  }
-  hasDescription = () => {
-    return !!this.props.description
-  }
-  handlePress = () => {
-    if (this.hasDescription()) {
-      this.setState({modalOpen: true})
-    }
-  }
-  closeModal = () => {
-    this.setState({modalOpen: false})
-  }
-  render() {
-    let infoPrompt = (<View style={{height: 14}} />)
-    if (this.props.description) {
-      infoPrompt = (
-        <Icon name="info" color="#00aeef" size={14} />
-      )
-    }
-    return (
-      <View style={this.props.style}>
-        <TouchableHighlight
-          onPress={this.handlePress}
-          underlayColor="#fff0"
-          activeOpacity={1}
-        >
-          <View>
-            {infoPrompt}
-            <Text>{this.props.children}</Text>
-          </View>
-        </TouchableHighlight>
-        {this.hasDescription() && (
-          <Modal
-            animationType="fade"
-            onRequestClose={this.closeModal}
-            visible={this.state.modalOpen}
-          >
-            <View style={styles.modal}>
-              <Text style={styles.modalHeader}>{this.props.children}</Text>
-              <ScrollView style={styles.modalBodyWrapper}>
-                <Text style={styles.modalBody}>{this.props.description}</Text>
-              </ScrollView>
-              <View style={styles.modalControls}>
-                <Button title="Close" onClick={this.closeModal} />
-              </View>
-            </View>
-          </Modal>
-        )}
-      </View>
-    )
-  }
-}
-
 const styles = StyleSheet.create({
-  modal: {
+  modalContainer: {
     flex: 1,
-    padding: 20,
-    paddingTop: 40,
-    paddingBottom: 40,
     flexDirection: 'column',
     backgroundColor: '#fff',
+  },
+  modal: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 20,
   },
   modalHeader: {
     fontWeight: 'bold',
@@ -85,4 +39,59 @@ const styles = StyleSheet.create({
   },
 })
 
-export default InfoLabel
+function InfoLabel(props) {
+  const [modalOpen, setModalOpen] = useState(false)
+  const hasDescription = () => {
+    return !!props.description
+  }
+  const handlePress = () => {
+    if (hasDescription()) {
+      setModalOpen(true)
+    }
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+  let infoPrompt = null
+  if (props.description) {
+    infoPrompt = (
+      <Icon name="info" color="#00aeef" size={14} />
+    )
+  }
+  return (
+    <View style={props.style}>
+      <TouchableHighlight
+        onPress={handlePress}
+        underlayColor="#fff0"
+        activeOpacity={1}
+      >
+        <Text>
+          {infoPrompt}
+          {!!infoPrompt && ' '}
+          {props.children}
+        </Text>
+      </TouchableHighlight>
+      {hasDescription() && (
+        <Modal
+          animationType="fade"
+          onRequestClose={closeModal}
+          visible={modalOpen}
+        >
+          <SafeAreaView style={styles.modalContainer}>
+            <View style={styles.modal}>
+              <Text style={styles.modalHeader}>{props.children}</Text>
+              <ScrollView style={styles.modalBodyWrapper}>
+                <Text style={styles.modalBody}>{props.description}</Text>
+              </ScrollView>
+              <View style={styles.modalControls}>
+                <Button title="Close" onClick={closeModal} />
+              </View>
+            </View>
+          </SafeAreaView>
+        </Modal>
+      )}
+    </View>
+  )
+}
+
+export default React.memo(InfoLabel)
