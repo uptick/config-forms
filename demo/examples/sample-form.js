0a640e70baa036daff89af326a076f65a24937c3
Michael Boreham <bor3ham@gmail.com>
2020-11-09 12:13:49 +1100

    Link to sample json and show value

--- demo/examples/sample-form.js
+++ demo/examples/sample-form.js
@@ -4,10 +4,15 @@ import WebRenderer from 'config-forms-react'
 
 import SampleForm from '../../docs/sample-form.json'
 
+const HOSTED_SAMPLE_FORM_URL = (
+  'https://github.com/uptick/config-forms/blob/master/docs/sample-form.json'
+)
+
 function DemoSampleForm(props) {
   const [value, setValue] = useState({})
   return (
     <>
+      <p><a href={HOSTED_SAMPLE_FORM_URL}>View the contents of the sample form on GitHub</a>.</p>
       <WebRenderer
         config={SampleForm}
         value={value}
@@ -15,7 +20,8 @@ function DemoSampleForm(props) {
           setValue(newValue)
         }}
       />
-      <p>Form value: "{JSON.stringify(value)}"</p>
+      <p>Form value:</p>
+      <pre>{JSON.stringify(value, null, 2)}</pre>
     </>
   )
 }
