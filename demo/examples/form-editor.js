66f4532e74bd38013d37b6a75f5617ea95f152dc
Michael Boreham <bor3ham@gmail.com>
2020-11-12 17:54:38 +1100

    Polish the editor demo a bit

--- demo/examples/form-editor.js
+++ demo/examples/form-editor.js
@@ -4,8 +4,10 @@ import WebEditor, { Palette } from 'config-forms-react-editor'
 import WebRenderer from 'config-forms-react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Button } from 'mireco'
 
 function DemoFormEditor(props) {
+  const [tab, setTab] = useState('editor')
   const [config, setConfig] = useState({
     fields: {
       test_text_1: {
@@ -34,25 +36,51 @@ function DemoFormEditor(props) {
   const [demoValue, setDemoValue] = useState({})
   return (
     <>
-      <DndProvider backend={HTML5Backend}>
-        <Palette />
-        <WebEditor
+      <div style={{textAlign: 'center', margin: '2rem 0'}}>
+        <Button
+          onClick={() => {setTab('editor')}}
+          className={tab === 'editor' ? '' : 'content outline'}
+        >
+          Editor
+        </Button>
+        {' '}
+        <Button
+          onClick={() => {setTab('config')}}
+          className={tab === 'config' ? '' : 'content outline'}
+        >
+          Config
+        </Button>
+        {' '}
+        <Button
+          onClick={() => {setTab('preview')}}
+          className={tab === 'preview' ? '' : 'content outline'}
+        >
+          Preview
+        </Button>
+      </div>
+      {tab === 'editor' && (
+        <DndProvider backend={HTML5Backend}>
+          <Palette />
+          <WebEditor
+            config={config}
+            onChange={(newConfig) => {
+              setConfig(newConfig)
+            }}
+          />
+        </DndProvider>
+      )}
+      {tab === 'config' && (
+        <pre>{JSON.stringify(config, null, 2)}</pre>
+      )}
+      {tab === 'preview' && (
+        <WebRenderer
           config={config}
-          onChange={(newConfig) => {
-            setConfig(newConfig)
+          value={demoValue}
+          onChange={(newValue) => {
+            setDemoValue(newValue)
           }}
         />
-      </DndProvider>
-      <hr />
-      <pre>{JSON.stringify(config, null, 2)}</pre>
-      <hr />
-      <WebRenderer
-        config={config}
-        value={demoValue}
-        onChange={(newValue) => {
-          setDemoValue(newValue)
-        }}
-      />
+      )}
     </>
   )
 }
